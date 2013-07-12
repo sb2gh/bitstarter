0ad02ec25951f0767c1811d6a0ebba810a63ce85
Stephane Boccara <boccaras@gmail.com>
2013-07-12 19:38:30 +0000

    Update 3 web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var content = fs.readFileSync('index.html', 'utf-8')';
+  response.send(content);//'Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
