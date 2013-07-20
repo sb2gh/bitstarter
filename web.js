cfd351312dcae4e03b2a1b8676762bd042480b4b
Ubuntu <ubuntu@ip-172-31-7-231.us-west-1.compute.internal>
2013-07-20 05:41:19 +0000

    web.js

--- web.js
+++ web.js
@@ -2,13 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/bootstrap'));
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
   response.send(buf.toString());//'Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
